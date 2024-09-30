715fe01587d85910d36260e514998bf3ce6c791e
Milena Lara <milenalrf@gmail.com>
2024-09-30 11:21:01 -0300

    created Spring Boot project

---
+++ project/code/sistema-moedas-api/src/main/java/br/pucminas/sistema_moedas_api/SistemaMoedasApiApplication.java
@@ -0,0 +1,13 @@
+package br.pucminas.sistema_moedas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMoedasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMoedasApiApplication.class, args);
+	}
+
+}
---
+++ project/code/sistema-moedas-api/src/test/java/br/pucminas/sistema_moedas_api/SistemaMoedasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucminas.sistema_moedas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMoedasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
