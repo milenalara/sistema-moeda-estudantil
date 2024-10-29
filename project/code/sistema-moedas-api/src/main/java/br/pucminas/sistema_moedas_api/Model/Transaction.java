15bb166499d31642e3c1dd71c8025efbeb144270
Gustavo Menezes Barbosa <gustavomb1313@gmail.com>
2024-10-29 16:06:16 -0300

    Add files via upload

---
+++ project/code/sistema-moedas-api/src/main/java/br/pucminas/sistema_moedas_api/Model/Transaction.java
@@ -0,0 +1,35 @@
+package br.pucminas.sistema_moedas_api.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @Column(nullable = false)
+    private String description;
+}
