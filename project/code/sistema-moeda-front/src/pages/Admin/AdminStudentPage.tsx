74525aa6acee423f74f09d01bb40490ef2e77698
Milena Lara <milenalrf@gmail.com>
2024-11-03 18:15:31 -0300

    organizando os componentes

--- project/code/sistema-moeda-front/src/pages/Admin/AdminStudentPage.tsx
+++ project/code/sistema-moeda-front/src/pages/Admin/AdminStudentPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import DataTable from "../components/DataTable";
-import AdminAppBar from "../components/AdminAppBar";
+import DataTable from "./components/DataTable";
+import AdminAppBar from "./components/AdminAppBar";
 
 const AdminStudentPage: React.FC = () => {
   return (
---
+++ project/code/sistema-moeda-front/src/pages/Student/StudentPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StudentAppBar from "./components/StudentAppBar";
+
+const StudentPage = () => {
+  return (
+    <>
+      <StudentAppBar />
+    </>
+  );
+};
+
+export default StudentPage;
